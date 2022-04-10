4d3c8def4b2b70d9b83ed05718a1b2295849da0c
limitpointinf0 <coneal0523@protonmail.com>
2022-04-11 06:04:49 +0700

    [ADD] control panel

--- dsociety-chat/src/components/chat/Chat.jsx
+++ dsociety-chat/src/components/chat/Chat.jsx
@@ -1,8 +1,15 @@
 import { Fragment, useEffect, useState, useRef } from "react";
 import { Box } from "@mui/system";
-import { Container, Paper, Typography, Divider, Grid, List, ListItem, ListItemText, FormControl, TextField, IconButton } from "@mui/material";
+import { MenuList, MenuItem, Container, Paper, Typography, Divider, Grid, List, ListItem, ListItemIcon, ListItemText, FormControl, TextField, IconButton } from "@mui/material";
 import './Chat.css';
 import SendIcon from '@mui/icons-material/Send'
+import ContentCut from '@mui/icons-material/ContentCut';
+import ContentCopy from '@mui/icons-material/ContentCopy';
+import ContentPaste from '@mui/icons-material/ContentPaste';
+import Cloud from '@mui/icons-material/Cloud';
+import TokenIcon from '@mui/icons-material/Token';
+import MeetingRoomIcon from '@mui/icons-material/MeetingRoom';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 export default function Chat({
     user, 
@@ -20,6 +27,8 @@ export default function Chat({
     
     const [chatMessages, setChatMessages] = useState([]);
     const [message, setMessage] = useState("");
+
+    const [roomId, setRoomId] = useState("");
 
 
     useEffect(() => {
@@ -85,42 +94,80 @@ export default function Chat({
 
     return (
         <Fragment>
-            <Container>
-                <Paper elevation={5}>
-                    <Box p={3}>
-                        <Typography variant="h6" gutterBottom>
-                            General
-                        </Typography>
-                        <Divider />
-                        <Grid container spacing={3} alignItems="center">
-                            <Grid id="chat-window" xs={12} item>
-                                <List id="chat-window-messages">
-                                    {listChatMessages}
-                                    <ListItem ref={scrollBottomRef}></ListItem>
-                                </List>
+            <Container disableGutters>
+                <Grid container spacing={1} alignItems="center" alignItems="stretch">
+                    <Grid id="chat" sm={9} xs={12} item>
+                        <Paper elevation={5}>
+                            <Box p={3}>
+                                <Typography variant="h6" gutterBottom>
+                                    General
+                                </Typography>
+                                <Divider />
+                                <Grid container spacing={1} alignItems="center">
+                                    <Grid id="chat-window" xs={12} item>
+                                        <List id="chat-window-messages">
+                                            {listChatMessages}
+                                            <ListItem ref={scrollBottomRef}></ListItem>
+                                        </List>
 
-                            </Grid>
-                            <Grid xs={10} item>
-                                <FormControl fullWidth>
-                                    <TextField 
-                                        onChange={handleMessageChange}
-                                        onKeyDown={handleEnterKey}
-                                        value={message}
-                                        label="Type your message..."
-                                        variant="outlined"/>
-                                </FormControl>
-                            </Grid>
-                            <Grid xs={2}item>
-                                <IconButton
-                                    onClick={sendMessage}
-                                    aria-label="send"
-                                    color="primary">
-                                        <SendIcon />
-                                </IconButton>
-                            </Grid>
-                        </Grid>
-                    </Box>
-                </Paper>
+                                    </Grid>
+                                    <Grid xs={11} item>
+                                        <FormControl fullWidth>
+                                            <TextField 
+                                                onChange={handleMessageChange}
+                                                multiline={true}
+                                                rows="2"
+                                                value={message}
+                                                label="Type your message..."
+                                                variant="outlined"/>
+                                        </FormControl>
+                                    </Grid>
+                                    <Grid xs={1}item>
+                                        <IconButton
+                                            onClick={sendMessage}
+                                            aria-label="send"
+                                            color="primary">
+                                                <SendIcon />
+                                        </IconButton>
+                                    </Grid>
+                                </Grid>
+                            </Box>
+                        </Paper>
+                    </Grid>
+                    <Grid id="faucet" sm={3} xs={12} item>
+                        <Paper elevation={5}>
+                            <Box p={3}>
+                                <Typography variant="h6" gutterBottom>
+                                    Control Panel
+                                </Typography>
+                                <Divider /> 
+                                <Grid container spacing={1} alignItems="center">
+                                    <MenuList>
+                                        <MenuItem onClick={()=>alert('hello')}>
+                                            <ListItemIcon>
+                                                <TokenIcon fontSize="small" />
+                                            </ListItemIcon>
+                                            <ListItemText>Request DST</ListItemText>
+                                        </MenuItem>
+                                        <MenuItem>
+                                            <ListItemIcon>
+                                                <MeetingRoomIcon fontSize="small" />
+                                            </ListItemIcon>
+                                            <ListItemText>Create Room</ListItemText>
+                                        </MenuItem>
+                                        <MenuItem>
+                                            <ListItemIcon>
+                                                <ArrowForwardIosIcon fontSize="small" />
+                                            </ListItemIcon>
+                                            <ListItemText>Join Room</ListItemText>
+                                        </MenuItem>
+                                    </MenuList>   
+                                </Grid>
+                                <Divider />
+                            </Box>
+                        </Paper>
+                    </Grid>
+                </Grid>
             </Container>
         </Fragment>
     )
